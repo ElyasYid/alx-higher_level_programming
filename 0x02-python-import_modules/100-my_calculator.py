a89983c90eb11bf87bae72becc55cc57f2bfcf08
ElyasYid <elyassayle27@gmail.com>
2023-11-30 01:47:39 -0800

    stupid pycodestyle is stupid

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,16 +3,17 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    x = sys.argv[2]
+
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operators = {"+": add, "-": sub, "*": mul, "/": div}
-    if x not in list(operators.keys()):
+    if sys.argv[2] not in list(operators.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, x, b, operators[x](a, b)))
+    result = operators[sys.argv[2]](a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
